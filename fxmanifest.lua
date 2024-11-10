f58f21e9848bf92d6af7ae50e45b3c13b28839ef
huh <itzcbuchbergmc@gmail.com>
2024-11-10 15:37:40 +0100

    Hello world

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'TH'
+description 'TH - Bandesystem'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+}
+
+dependencies {
+    'ox_lib'
+}
